20fb45eb9025b226a93c31129d6a50199173ff13
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-23 13:42:49 +0545

    finish: query method use in mongoose

---
+++ basics_Schema/query.js
@@ -0,0 +1,28 @@
+import mongoose, { model } from 'mongoose'
+import { connectDb } from '../config/connect.db.js';
+connectDb()
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+},{
+    query:{
+        byName(name){
+           return this.where({name:new RegExp(name,'i')});//?here, i will tell you where is this 'this' is pointing here so below
+        }
+    }
+})
+
+const User=mongoose.model('User',userSchema)
+
+//   await User.find()//! it will return all the document and now if i do .byname() method i have defined under the query object then it will point that return object and chain the qurey like where name:'ram' and execute that data.
+
+//! so to add the query we can do like that
+
+const user= new User({name:'prajwol',email:'hello@gmail.com',password:'rampleasehelpme'})
+
+console.log(user)
+   const data=await User.find().byName('prajwol')
+   console.log(data)
+await user.save()
